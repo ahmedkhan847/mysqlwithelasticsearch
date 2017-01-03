7887888017d53119bef18fe24502c52f778b436a
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-03 15:33:26 +0500

    adding index file

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ require "vendor/autoload.php";
 use SearchElastic\SearchElastic;
 
 $elastic = new SearchElastic();
+$con = new mysqli("localhost","root","","cms");
+$result = $elastic->insertAllData($con);
 
