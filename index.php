9a7f284c5b73a8aee7e299bb5241789dad952278
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-23 20:42:10 +0500

    turning into a complete composer package,.

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 require "vendor/autoload.php";
+include "config.php";
 use SearchElastic\SearchElastic;
 
-$elastic = new SearchElastic();
+$elastic = new SearchElastic($config);
 $con = new mysqli("localhost","root","","cms");
 $result = $elastic->insertAllData($con);
 
+echo '<pre>';
+print_r($result);
+echo '</pre>';
